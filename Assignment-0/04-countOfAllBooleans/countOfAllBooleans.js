b8b1cd9fa40634a19515586507c0bc982f1b952e
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-04 22:35:57 -0500

    test04 done

--- Assignment-0/04-countOfAllBooleans/countOfAllBooleans.js
+++ Assignment-0/04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,12 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  let numBoo = 0;
+  arr.forEach(element => {
+    if (typeof element == "boolean") {
+      numBoo++
+    }
+  });
+  return numBoo
 }
-
+countOfAllBooleans([true, false, true, false, true, 7])
 // Do not edit this line;
 module.exports = countOfAllBooleans;
