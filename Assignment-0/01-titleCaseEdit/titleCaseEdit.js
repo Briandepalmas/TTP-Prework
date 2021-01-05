503e1345f54ea26288afcf1a4f2a1789bd60bfc9
Briandepalmas <Student_GA110@Student-GA110.local>
2021-01-04 20:59:04 -0500

    test01 passed

--- Assignment-0/01-titleCaseEdit/titleCaseEdit.js
+++ Assignment-0/01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,18 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  let tArr = title.split(" ")
+  let newTitle = []
+
+  tArr.forEach(word => {
+    let cap = word[0].toUpperCase() + word.substring(1)
+    newTitle.push(cap)
+  })
+  newTitle = newTitle.join(" ")
+  
+  return newTitle;
+
 }
+
+titleCaseEdit("yo hago lo que me da la gana")
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
