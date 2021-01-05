41f9b28ebca2bcb902b27f990f41db61538a4d5d
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-04 23:00:08 -0500

    test05 done

--- Assignment-0/05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ Assignment-0/05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,14 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
-}
+  let numBoo = 0;
+  arr.forEach(element => {
+    if (typeof element == "boolean"||typeof element=="string") {
+      numBoo++
+    }
 
+  });
+  return numBoo 
+  
+}
+countOfAllBooleansAndStrings(["hey", "there", "hello", "there", "world", false])
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
