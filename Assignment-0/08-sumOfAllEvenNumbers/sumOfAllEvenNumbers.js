fc10e94e82bd653f6c0c64640196d755dfe42956
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-15 15:58:48 -0500

    8done

--- Assignment-0/08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ Assignment-0/08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,15 @@
-function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
-}
+let nums = [2, 3, 4, 5, 6, 7, 8, 9]
 
+function sumOfAllEvenNumbers(nums) {
+  let evenTotal = 0
+
+  nums.forEach(element => {
+    if (element % 2 == 0) {
+      evenTotal++
+    }
+  });
+  return evenTotal;
+}
+sumOfAllEvenNumbers(nums)
 // Do not edit this line;
 module.exports = sumOfAllEvenNumbers;
