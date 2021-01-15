184608ce066ab356cc9486acbb60b11e3d76eca8
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-15 16:04:07 -0500

    complete

--- Assignment-0/09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ Assignment-0/09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,16 @@
+let nums = [2, 3, 4, 5, 6, 7, 8, 9]
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  let oddTotal = 0
+
+  nums.forEach(element => {
+    if (element % 2 == 1) {
+      oddTotal++
+    }
+  });
+  return oddTotal;
 }
+sumOfAllOddNumbers(nums)
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
