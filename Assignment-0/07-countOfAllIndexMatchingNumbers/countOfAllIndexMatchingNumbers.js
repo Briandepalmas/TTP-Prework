cb117681b522f9ec6b50d65776d5c828554b0ad8
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-15 15:17:29 -0500

    test7complete

--- Assignment-0/07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ Assignment-0/07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,6 +1,16 @@
+let nums = [0, 1, 2, 5, 4];
+
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  let match=0
+  for (i = 0; i < nums.length; i++) {
+    
+    if(nums[i]==i){
+      match++
+    }   
+  }
+  return match;
 }
-
+countOfAllIndexMatchingNumbers(nums)
 // Do not edit this line;
 module.exports = countOfAllIndexMatchingNumbers;
