ef71879fc3be900b1ff5c6db6da979939da1e35a
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-15 14:45:52 -0500

    test6done

--- Assignment-0/06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ Assignment-0/06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,17 @@
+let nums = [1, 2, 3, 4, 5]
+let target = 9;
+
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
-}
 
+  lessThanTarget = 0;
+  nums.forEach(element => {
+    if (element < target) {
+      lessThanTarget++;
+    }
+  });
+  return lessThanTarget;
+}
+countOfAllNumbersSmallerThanTarget(nums,target)
 // Do not edit this line;
 module.exports = countOfAllNumbersSmallerThanTarget;
