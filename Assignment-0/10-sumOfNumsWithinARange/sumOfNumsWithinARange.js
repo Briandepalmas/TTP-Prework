5b05b6aca0f53dd655614404a75cd1d647a95492
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-15 16:16:25 -0500

    10done

--- Assignment-0/10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ Assignment-0/10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,6 +1,17 @@
+let nums = [1, 2, 3, 4, 5, 6, 7]
+let start = 4;
+let end = 7;
+
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let range = 0
+  nums.forEach(element => {
+    if (element >= start && element <= end) {
+      range++
+    }
+  })
+  return range
 }
-
+sumOfNumsWithinARange(nums,start,end)
 // Do not edit this line;
 module.exports = sumOfNumsWithinARange;
