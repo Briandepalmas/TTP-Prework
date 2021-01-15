d2043ab301edbc49d50ef591a7396be49052519a
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-15 18:59:37 -0500

    eleven done

--- Assignment-0/11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ Assignment-0/11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,10 @@
+let nums = [1, 4, 6, 33, 54, 99, 12, 85]
+
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  let sum = Math.min(...nums) + Math.max(...nums)
+  return sum;
 }
-
+sumOfMinimumAndMaximum(nums)
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
