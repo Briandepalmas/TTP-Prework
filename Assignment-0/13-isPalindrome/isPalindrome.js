507accf256759f75e6b42ac1cbdb3a8bd2a07aa5
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-15 21:05:23 -0500

    test13 complete

--- Assignment-0/13-isPalindrome/isPalindrome.js
+++ Assignment-0/13-isPalindrome/isPalindrome.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
   // Insert code here;
-}
 
+  let check = word.split("")
+  check.reverse()
+  let palin = check.join("")
+  if (palin == word) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+isPalindrome("sabrina")
 // Do not edit this line;
 module.exports = isPalindrome;
