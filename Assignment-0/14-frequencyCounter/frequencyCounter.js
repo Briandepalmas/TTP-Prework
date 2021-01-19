e7516c23512e0c9013656b0793d2d20f56b72e23
Briandepalmas <Student_GA110@Student-GA110.home>
2021-01-19 13:17:09 -0500

    done

--- Assignment-0/14-frequencyCounter/frequencyCounter.js
+++ Assignment-0/14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,23 @@
 function frequencyCounter(word) {
-  // Insert code here;
-}
+  let arr = word.split("")
+  let obj = {}
+  for (i = 0; i <= word.length - 1; i++) {
+    obj[word[i]] = 0;
 
+  }
+
+  arr.forEach(element => {
+    if (obj[element] == obj[element]) {
+      obj[element] += 1;
+    }
+
+  });
+
+  console.log(obj)
+  return obj
+
+
+}
+frequencyCounter("mississippi")
 // Do not edit this line;
 module.exports = frequencyCounter;
